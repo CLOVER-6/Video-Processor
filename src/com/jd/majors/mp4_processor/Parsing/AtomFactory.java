ba6e928f258520829903140934dea440231063e0
CLOVER-6 <89226564+CLOVER-6@users.noreply.github.com>
2026-02-02 11:19:13 +0000

    Delete src/com/jd/majors/mp4_processor directory
    Deleted unnecessary directory likely from editing and uploading from school

--- src/com/jd/majors/mp4_processor/Parsing/AtomFactory.java
+++
@@ -1,9 +0,0 @@
-package com.jd.majors.mp4_processor.Parsing;
-
-import com.jd.majors.mp4_processor.AtomClasses.Interfaces.Box;
-
-@FunctionalInterface
-public interface AtomFactory 
-{
-	Box createAtom(int size, String name, byte[] payload);
-}
